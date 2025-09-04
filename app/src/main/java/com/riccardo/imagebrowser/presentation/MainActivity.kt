31d2c40c4e3802b128479b22495848cfd7019e17
RiccardoCestaro <riccardo.cestaro@outlook.it>
2025-09-04 17:36:31 +0200

    Move MainActivity to presentation

---
+++ app/src/main/java/com/riccardo/imagebrowser/presentation/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.riccardo.imagebrowser.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.riccardo.imagebrowser.presentation.ui.theme.ImageBrowserTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ImageBrowserTheme {
+
+                val navController = rememberNavController()
+
+                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = SearchScreen,
+                        modifier = Modifier.Companion.padding(innerPadding)
+                    ) {
+                        composable<SearchScreen> {
+                            SearchScreen(
+                                query = "",
+                                onQueryChange = {},
+                                onSearch = {},
+                                searchResults = emptyList(),
+                                onResultClick = {})
+                        }
+                        composable<DetailsScreen> {
+                            DetailsScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
