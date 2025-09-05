547962db8d4016979243a1d250fc3a86a14ea825
RiccardoCestaro <riccardo.cestaro@outlook.it>
2025-09-05 09:51:25 +0200

    feat: avoid to collect and emit again

--- app/src/main/java/com/riccardo/imagebrowser/domain/usecases/SearchUseCase.kt
+++ app/src/main/java/com/riccardo/imagebrowser/domain/usecases/SearchUseCase.kt
@@ -1,26 +1,26 @@
 package com.riccardo.imagebrowser.domain.usecases
 
+import android.util.Log
 import com.riccardo.imagebrowser.data.model.SearchResponse
 import com.riccardo.imagebrowser.domain.repository.SearchRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class SearchUseCase @Inject constructor(
     private val repository: SearchRepository
 ) {
 
-    operator fun invoke(query: String): Flow<SearchResponse> = flow {
-        repository.searchImages(query).collect {
-            it.onSuccess { content ->
-                emit(content)
+    suspend operator fun invoke(query: String): Flow<SearchResponse> =
+        repository.searchImages(query)
+            .map { result ->
+                result.onSuccess { content ->
+                    Log.d("SearchUseCase", "invoke: $content")
+                }
+                result.getOrThrow()
             }
-            it.onFailure { e ->
+            .catch { e ->
                 e.printStackTrace()
             }
-        }
-    }.catch {
-        it.printStackTrace()
-    }
 }
