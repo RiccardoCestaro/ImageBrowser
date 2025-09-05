f7d1bb3f6f5b9a35f79d17da4acc1ab93b3744d6
RiccardoCestaro <riccardo.cestaro@outlook.it>
2025-09-05 09:50:10 +0200

    fix: use query instead of path for query parameter

--- app/src/main/java/com/riccardo/imagebrowser/data/service/SearchApiService.kt
+++ app/src/main/java/com/riccardo/imagebrowser/data/service/SearchApiService.kt
@@ -3,10 +3,10 @@ package com.riccardo.imagebrowser.data.service
 import com.riccardo.imagebrowser.data.model.SearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SearchApiService {
 
     @GET("search/photos")
-    suspend fun search(@Path("query") query: String): Response<SearchResponse>
+    suspend fun search(@Query("query") query: String): Response<SearchResponse>
 }
